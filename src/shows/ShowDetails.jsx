59ee70e3ada7d1dcadf4304771feea10605498f0
Robert Hughes <rjh2869@gmail.com>
2026-09-16 09:53:25 -0500

    finished ShowDetails function

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,6 +1,17 @@
+import { useState } from "react";
+
+import EpisodeList from "../episodes/EpisodeList";
+import EpisodeDetails from "../episodes/EpisodeDetails";
+
 import "./shows.css";
 
 /** Allows users to browse through the episodes of the given show */
-export default function ShowDetails() {
+export default function ShowDetails({ show }) {
+  const [selectedEpisode, setSelectedEpisode] = useState();
+
+  if (!show) {
+    return <p>Please Select a show to learn more.</p>;
+  }
+
   return <div className="show-details"></div>;
 }
